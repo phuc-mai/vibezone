3204f8f86aac52397fcaf1d40ced09de3e5758a8
Phuc Mai <mariamaihong1309@gmail.com>
2023-12-30 01:11:08 -0500

    topbar

--- components/layout/LeftSideBar.jsx
+++ components/layout/LeftSideBar.jsx
@@ -1,9 +1,10 @@
-"use client"
+"use client";
 
 import {
   SignOutButton,
   SignedIn,
   UserButton,
+  useClerk,
   useUser,
 } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
@@ -15,26 +16,30 @@ import { useEffect, useState } from "react";
 import Loader from "@components/Loader";
 
 const LeftSideBar = () => {
-  const { user, isLoaded } = useUser()
-  
-  const [loading, setLoading] = useState(true)
+  const { signOut } = useClerk();
 
-  const [userData, setUserData] = useState({})
+  const { user, isLoaded } = useUser();
+
+  const [loading, setLoading] = useState(true);
+
+  const [userData, setUserData] = useState({});
 
   const getUser = async () => {
-    const response = await fetch(`/api/user/${user.id}`)
-    const data = await response.json()
-    setUserData(data)
-    setLoading(false)
-  }
+    const response = await fetch(`/api/user/${user.id}`);
+    const data = await response.json();
+    setUserData(data);
+    setLoading(false);
+  };
 
   useEffect(() => {
     if (user) {
-      getUser()
+      getUser();
     }
-  }, [user])
-  
-  return loading || !isLoaded ? <Loader /> : (
+  }, [user]);
+
+  return loading || !isLoaded ? (
+    <Loader />
+  ) : (
     <div className="h-screen left-0 top-0 sticky overflow-auto px-10 py-6 flex flex-col gap-6 max-md:hidden 2xl:w-[350px] pr-20 custom-scrollbar">
       <Link href="/">
         <Image src="/assets/logo.png" alt="logo" width={200} height={200} />
@@ -51,7 +56,9 @@ const LeftSideBar = () => {
               className="rounded-full"
             />
           </Link>
-          <p className="text-small-bold">{userData?.firstName} {userData?.lastName}</p>
+          <p className="text-small-bold">
+            {userData?.firstName} {userData?.lastName}
+          </p>
         </div>
         <div className="flex text-light-1 justify-between">
           <div className="flex flex-col items-center">
@@ -80,8 +87,9 @@ const LeftSideBar = () => {
         <p className="text-light-1 text-body-bold">Manage Account</p>
       </div>
 
+      {/* Clerk Docs for Sign Out Button - https://clerk.com/docs/components/unstyled/sign-out-button#sign-out-button */}
       <SignedIn>
-        <SignOutButton>
+        <SignOutButton signOutCallback={() => signOut()}>
           <div className="flex cursor-pointer gap-4 items-center">
             <Logout sx={{ color: "white", fontSize: "32px" }} />
             <p className="text-body-bold text-light-1">Log Out</p>
--- components/layout/TopBar.jsx
+++ components/layout/TopBar.jsx
@@ -1,17 +1,40 @@
 "use client";
 
-import { useState } from "react";
-import { Add, Logout, Search } from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import { Add, Logout, Person, Search } from "@mui/icons-material";
 import { useRouter } from "next/navigation";
-import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { SignOutButton, SignedIn, UserButton, useUser } from "@clerk/nextjs";
 import Link from "next/link";
 import Image from "next/image";
+import { dark } from "@clerk/themes";
+import Loader from "@components/Loader";
 
 const TopBar = () => {
+  const { user, isLoaded } = useUser();
+
+  const [loading, setLoading] = useState(true);
+
+  const [userData, setUserData] = useState({});
+
+  const getUser = async () => {
+    const response = await fetch(`/api/user/${user.id}`);
+    const data = await response.json();
+    setUserData(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (user) {
+      getUser();
+    }
+  }, [user]);
+
   const router = useRouter();
   const [search, setSearch] = useState("");
 
-  return (
+  return !isLoaded || loading ? (
+    <Loader />
+  ) : (
     <div className="flex justify-between items-center mt-6">
       <div className="relative">
         <input
@@ -21,7 +44,10 @@ const TopBar = () => {
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <Search className="search-icon" onClick={() => router.push(`/search/posts/${search}`)} />
+        <Search
+          className="search-icon"
+          onClick={() => router.push(`/search/posts/${search}`)}
+        />
       </div>
 
       <button
@@ -31,24 +57,12 @@ const TopBar = () => {
         <Add /> <p>Create A Post</p>
       </button>
 
-      <div className="flex gap-3">
-        <SignedIn>
-          <SignOutButton>
-            <div className="flex cursor-pointer items-center md:hidden">
-              <Logout sx={{ color: "white", fontSize: "32px" }} />
-            </div>
-          </SignOutButton>
-        </SignedIn>
-
-        <Link href="/">
-          <Image
-            src="/assets/phucmai.png"
-            alt="profile photo"
-            width={50}
-            height={50}
-            className="rounded-full md:hidden"
-          />
+      <div className="flex gap-4 md:hidden">
+        <Link href={`/profile/${userData._id}/posts`}>
+          <Person sx={{ fontSize: "35px", color: "white" }} />
         </Link>
+
+        <UserButton appearance={{ baseTheme: dark }} />
       </div>
     </div>
   );
